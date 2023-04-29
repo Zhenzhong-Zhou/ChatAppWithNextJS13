04bbbbf004a9e7c59afe188b2365db0f4c81109b
Zhenzhong Zhou <bob0823.zhou@gmail.com>
2023-04-29 11:48:24 -0600

    Connect with database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import {Inter} from 'next/font/google'
+import {db} from "@/lib/db";
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default async function Home() {
+    await db.set("hello", "hello");
     return (
         <div className={"text-red-500"}>
             Hello World
