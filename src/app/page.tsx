0e81e0947b21678883dc693f6a29198cd6a2196b
Zhenzhong Zhou <bob0823.zhou@gmail.com>
2023-04-28 21:47:18 -0600

    Reformat codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <div className={"text-red-500"}>
-      Hello World
-    </div>
-  )
+    return (
+        <div className={"text-red-500"}>
+            Hello World
+        </div>
+    )
 }
