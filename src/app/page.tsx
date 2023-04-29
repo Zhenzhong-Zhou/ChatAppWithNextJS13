801982cc2e3d13ad2291ec6ec2f35f41cbeb29f9
Zhenzhong Zhou <bob0823.zhou@gmail.com>
2023-04-29 12:32:48 -0600

    Implement auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import {Inter} from 'next/font/google'
 import {db} from "@/lib/db";
+import Button from "@/components/ui/Button";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default async function Home() {
     await db.set("hello", "hello");
     return (
-        <div className={"text-red-500"}>
-            Hello World
-        </div>
+        <Button variant={"ghost"}>Hello World</Button>
     )
 }
